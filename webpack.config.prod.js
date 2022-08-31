f07b6d2248c67a7aa37dd6b416b9729bee30d676
Wojciech Glegola <wglegola@gmail.com>
2022-08-31 19:30:04 +0200

    added webpack & bundled js code

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,27 +1,30 @@
-const path = require('path');
-const CleanPlugin = require('clean-webpack-plugin');
+const path = require("path");
+const CleanPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/app.ts',
+  mode: "production",
+  entry: "./src/app.ts",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  devtool: 'none',
+  devtool: false,
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
+  // devServer: {
+  //   static: {
+  //     directory: path.join(__dirname, "/"),
+  //   },
+  // },
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: [".ts", ".js"],
   },
-  plugins: [
-    new CleanPlugin.CleanWebpackPlugin()
-  ]
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
